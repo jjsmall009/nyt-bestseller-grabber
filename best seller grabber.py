7aa9401004a95edb91d80ed4aaf03988f12712c6
jjsmall009 <jjsmall009@gmail.com>
2021-09-10 20:37:53 -0700

    Initial commit. Decided to make an actual project out of this and improve the initial work I've already done. More to come.

---
+++ best seller grabber.py
@@ -0,0 +1,177 @@
+#################################################################################################
+"""New York Times Bestsellers List Grabber
+
+Author: JJ Small
+Date: 9/6/21
+Company: Anacortes Public Library
+
+This script will create an excel spreadsheet for the sole purpose of printing out and displaying
+in the library. As of now it only processes fiction and nonfiction data.
+
+Using the NYT API we grab data from certain endpoints and update a template excel file with the 
+current NYT bestseller information. If you're reading this then I must be dead...
+"""
+#################################################################################################
+
+from xlsxwriter import Workbook
+from datetime import datetime
+from io import BytesIO
+from requests import get
+
+# Specific to jjsmall009 NYT API account
+API_KEY = "wup3sLrAcPAxjDtA2M4Tnu124slqmeG1"
+API_URL = "https://api.nytimes.com/svc/books/v3/"
+DATE = datetime.today()
+ 
+def get_list_data(book_url):
+    """
+    Fetch all book data from the specified url and then grab the data we want from it. 
+
+    Parameters:
+        book_url (string): The API url based on the type of book you're requesting. Fiction, etc.
+    
+    Returns:
+        List: Returns a list of books, where each book is a dictionary of certain parameters and values
+    """
+
+    request = get(url=book_url)
+    request.raise_for_status()
+
+    try:
+        print("Attempting to grab data from the NYT API...")
+        data = request.json()["results"]["books"]
+    except KeyError as e:
+        # If for some reason we get data back that isn't in the correct format we'll crash
+        print("Oops... the data isn't correct. Use the correct book type")
+        print(e)
+        exit()
+    else:
+        # Grab the data we want and not all of the other useless junk
+        final_data = []
+        keys = ["rank", "description", "title", "author", "book_image"]
+        for book in data:
+            book_dict_data = {info:book[info] for info in keys}
+            final_data.append(book_dict_data)
+        
+        print("Finished grabbing data....")
+        return final_data
+
+def create_spreadsheet(file_name):
+    
+    bold = wb.add_format({"bold": True})
+
+    return wb
+
+def update_spreadsheet(book_data, sheet):
+    """
+    Update the specified sheet with the new data we get from the list API. Basically it
+    iterates over the list of books and will add in all of the new books data. 
+
+    Parameters:
+        book_data (list): The formatted list of books, where book = dict
+        sheet_name (sheet): The sheet object (Fiction, etc.)
+    """
+
+    print(f"Processing {sheet} data.......")
+    sheet.set_column(0,0, width=16.25)
+    sheet.set_column(16,16, width=14.2)
+    merge_format = wb.add_format({"valign": "vcenter", "text_wrap": True})
+
+    # Add first row header
+    sheet.set_row(0, height=40)
+    sheet.merge_range("A1:S1", "")
+    header_bold = wb.add_format({
+        "font_name": "Cooper Hewitt Book",
+        "font_size": 28,
+        "font_color": "#44546A",
+        "bold": True
+    })
+
+    header_style = wb.add_format({
+        "font_name": "Cooper Hewitt Book",
+        "font_size": 28,
+        "font_color": "#44546A"
+    })
+    sheet.write_rich_string(
+        "A1", 
+        header_bold,
+        "BEST SELLERS",
+        header_style,
+        " In the Anacortes Public Library Collection")
+
+    # Add second row with current date
+    sheet.set_row(1, height=95)
+    sheet.merge_range("A2:Q2", "")
+    date_header = wb.add_format({
+        "font_name": "Cooper Hewitt Book",
+        "font_size": 22,
+        "font_color": "#4472C4",
+        "align": "center",
+        "valign": "vcenter"
+    })
+    sheet.write(
+        "A2", 
+        f"The New York Times - Hardcover {sheet.get_name()} - {DATE.strftime('%B %d, %Y')}",
+        date_header)
+
+    # For each book in our list, add in the data to the proper cells and format accordingly
+    for index, book in enumerate(book_data):
+        status_style = wb.add_format({
+            "font_name": "Cooper Hewitt Book",
+            "font_size": 22,
+            "align": "center",
+            "valign": "vcenter",
+            "text_wrap": True
+        })
+
+        book_style = wb.add_format({
+            "font_name": "Cooper Hewitt Book",
+            "font_size": 22,
+        })
+
+        title_bold = wb.add_format({
+            "font_name": "Cooper Hewitt Book",
+            "font_size": 22,
+            "bold": True,
+        })
+        row = index + 2
+        sheet.set_row(row, height=125)
+        sheet.merge_range(row, 2, row, 15, "", merge_format)
+
+        sheet.write(row, 0, "-", status_style) # This is the "Have/On Order" field and we have to manually do this later
+        sheet.write(row, 1, book["rank"], status_style)
+        sheet.write_rich_string( # Format the book information with a bold title and such
+            row, 
+            2, 
+            title_bold,
+            f"{book['title']}",
+            book_style,
+            f" by {book['author']}\n{book['description']}",
+            merge_format)
+        
+        # Add in the image
+        r = get(url=book["book_image"])
+        url = "google.com"
+        image_file = BytesIO(r.content)
+        sheet.insert_image(row, 16, url, {"image_data": image_file, "x_scale": .32, "y_scale": .32})
+    print("Finished processing data...")
+        
+
+# Craft our endpoints
+fiction_endpoint = f"{API_URL}lists/current/hardcover-fiction.json?api-key={API_KEY}"
+non_endpoint = f"{API_URL}lists/current/hardcover-nonfiction.json?api-key={API_KEY}"
+
+# Get and process all of the data and do the stuff
+fiction_data = get_list_data(fiction_endpoint)
+non_data = get_list_data(non_endpoint)
+
+# Open up our template spreadsheet and update it with the current list of bestsellers
+new_file = f"results/{DATE.strftime('%B %d')} New York Bestsellers.xlsx"
+wb = Workbook(new_file)
+f_sheet = wb.add_worksheet("Fiction")
+nf_sheet = wb.add_worksheet("Nonfiction")
+
+update_spreadsheet(fiction_data, f_sheet)
+update_spreadsheet(non_data, nf_sheet)
+
+wb.close()
---
+++ openpyxl-version.py
@@ -0,0 +1,129 @@
+#################################################################################################
+"""New York Times Bestsellers List Grabber
+
+Author: JJ Small
+Date: 9/5/21
+Company: Anacortes Public Library
+
+This script will create an excel spreadsheet for the sole purpose of printing out and displaying
+in the library. As of now it only processes fiction and nonfiction data.
+
+Using the NYT API we grab data from certain endpoints and update a template excel file with the 
+current NYT bestseller information. If you're reading this then I must be dead...
+"""
+#################################################################################################
+
+from datetime import datetime
+import io
+import openpyxl
+import requests
+from requests import HTTPError
+
+# Specific to jjsmall009 NYT API account
+API_KEY = "wup3sLrAcPAxjDtA2M4Tnu124slqmeG1"
+API_URL = "https://api.nytimes.com/svc/books/v3/"
+DATE = datetime.today()
+ 
+def get_list_data(book_url):
+    """
+    Fetch all book data from the specified url and then grab the data we want from it. 
+
+    Parameters:
+        book_url (string): The API url based on the type of book you're requesting. Fiction, etc.
+    
+    Returns:
+        List: Returns a list of books, where each book is a dictionary of certain parameters and values
+    """
+
+    request = requests.get(url=book_url)
+    request.raise_for_status()
+
+    try:
+        print("Attempting to grab data from the NYT API...")
+        data = request.json()["results"]["books"]
+    except KeyError as e:
+        # If for some reason we get data back that isn't in the correct format we'll crash
+        print("Oops... the data isn't correct. Use the correct book type")
+        print(e)
+        exit()
+    else:
+        # Grab the data we want and not all of the other useless junk
+        final_data = []
+        keys = ["rank", "description", "title", "author", "book_image"]
+        for book in data:
+            book_dict_data = {info:book[info] for info in keys}
+            final_data.append(book_dict_data)
+        
+        print("Finished grabbing data....")
+        return final_data
+
+
+def open_spreadsheet(file_name):
+    """
+    Open up a spreadsheet and use it for updating the data
+    
+    Parameters:
+        file_name (string): The name of the template excel file
+
+    Returns:
+        Openpyxl Workbook: Returns this object full of fancy excel data
+    
+    """
+
+    return openpyxl.load_workbook(filename=file_name)
+
+
+def update_spreadsheet(book_data, workbook, sheet_name):
+    """
+    Update the specified workbook object with the new data we get from the list API. Basically it
+    iterates over the list of books and will modify the matching cells in the specified sheet to 
+    contain the new data. We later write the modified workbook to a new file for todays date.
+
+    Parameters:
+        book_data (list): The formatted list of books, where book = dict
+        workbook (Openpyxl Workbook): The template excel file in workbook format
+        sheet_name (string): The sheet name corresponds to the type of list (Fiction, etc.)
+    """
+
+    print(f"Processing {sheet_name} data.......")
+    sheet = workbook[sheet_name]
+
+    # Update second row with current date
+    sheet["A2"] = f"The New York Times - Hardcover {sheet_name} - {DATE.strftime('%B %d, %Y')}"
+
+    # For each row in the sheet, starting at the third row, update the contents of the row with
+    # the new book data we pulled in from the API.
+    for index, row in enumerate(sheet.iter_rows(min_row=3)):
+        book = book_data[index]
+        row[0].value = "" # This is the "Have/On Order" field and we have to manually do this later
+        row[1].value = book["rank"]
+        row[2].value = f"{book['title']} by {book['author']}\n{book['description']}"
+        
+        # Add in the image
+        r = requests.get(url=book["book_image"])
+        image_file = io.BytesIO(r.content)
+        img = openpyxl.drawing.image.Image(image_file)
+        img.width = 110
+        img.height = 165
+        spot = f"L{index + 3}"
+        sheet.add_image(img, spot)
+    print("Finished processing data...")
+        
+
+# Craft our endpoints
+fiction_endpoint = f"{API_URL}lists/current/hardcover-fiction.json?api-key={API_KEY}"
+non_endpoint = f"{API_URL}lists/current/hardcover-nonfiction.json?api-key={API_KEY}"
+
+# Get and process all of the data and do the stuff
+fiction_data = get_list_data(fiction_endpoint)
+non_data = get_list_data(non_endpoint)
+
+# Open up our template spreadsheet and update it with the current list of bestsellers
+workbook = open_spreadsheet("TEMPLATE.xlsx")
+update_spreadsheet(fiction_data, workbook, "Fiction")
+update_spreadsheet(non_data, workbook, "Nonfiction")
+
+# Write to new file with the new data
+new_file = f"{DATE.strftime('%B %d')} New York Bestsellers.xlsx"
+workbook.save(filename=new_file)
+workbook.close()
